fbb8d0b5ab7978b4023db124387646c2f92c39b8
Mz2022 <mz20220127@163.com>
2024-08-09 11:48:05 +0800

    完成登录页面

---
+++ src/components/RightContent/index.tsx
@@ -0,0 +1,32 @@
+import { QuestionCircleOutlined } from '@ant-design/icons';
+import '@umijs/max';
+export type SiderTheme = 'light' | 'dark';
+
+function UmiSelectLang(props: { style: { padding: number } }) {
+  return null;
+}
+
+export const SelectLang = () => {
+  return (
+    <UmiSelectLang
+      style={{
+        padding: 4,
+      }}
+    />
+  );
+};
+export const Question = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: 26,
+      }}
+      onClick={() => {
+        window.open('https://pro.ant.design/docs/getting-started');
+      }}
+    >
+      <QuestionCircleOutlined />
+    </div>
+  );
+};
